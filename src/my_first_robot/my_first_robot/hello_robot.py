8d9922d16fd647846cee7759b6a4b7bf5d51b5ac
Joshua Scattini <joshuascattini93@gmail.com>
2026-08-08 10:41:39 +0930

    Implemented long-running ROS node with timer callbacks

--- src/my_first_robot/my_first_robot/hello_robot.py
+++ src/my_first_robot/my_first_robot/hello_robot.py
@@ -4,13 +4,23 @@ from rclpy.node import Node
 class HelloRobot(Node):
     def __init__(self):
         super().__init__("hello_robot")
+        self.seconds_passed_since_init = 0 
         self.get_logger().info("Hello, Robot from MrBessy!")
+
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.seconds_passed_since_init += 1
+        self.get_logger().info(f"{self.seconds_passed_since_init} seconds have passed since initializing")
 
 def main(args=None):
     rclpy.init(args=args)
 
     node = HelloRobot()
 
+    rclpy.spin(node)
+
+    node.destroy_node()
     rclpy.shutdown()
 
     
